d659ef36729687d60e703ebfec949b6a550fb7bd
Rvillamizar492 <rvillamizar492@gmail.com>
2025-09-28 17:27:32 -0500

    Primer commit - AppCitasVeterinaria con Expo

---
+++ App.js
@@ -0,0 +1,56 @@
+import { Text, View, StyleSheet, Button, Pressable, Modal } from "react-native";
+import React, { useState } from "react";
+export default function App() {
+  const nuevaCitaHandler = () => {
+    console.log("Presionando");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Administrador de Citas</Text>
+      <Text style={styles.subtitulo}>Veterinaria Angelitos </Text>
+      <Pressable onPress={nuevaCitaHandler} style={styles.btn}>
+        <Text style={styles.btnNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+      <Modal animationType="slide" visible={false}>
+        <Text>Desde Modal</Text>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F3F4F6",
+    flex: 1,
+  },
+  titulo: {
+    textAlign: "center",
+    marginTop: 40,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#374151",
+  },
+  subtitulo: {
+    textAlign: "center",
+    fontSize: 25,
+    marginTop: 5,
+    color: "#6D28D9",
+    fontWeight: "600",
+    textTransform: "uppercase",
+  },
+  btn: {
+    backgroundColor: "#6D28D9",
+    padding: 15,
+    marginTop: 35,
+    marginHorizontal: "20",
+    borderRadius: 10,
+  },
+  btnNuevaCita: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "900",
+    textTransform: "uppercase",
+  },
+});
